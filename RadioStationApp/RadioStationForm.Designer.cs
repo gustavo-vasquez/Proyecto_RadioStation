dfcd18af1b0b760c49b9105da45f38d8c1f15b9d
Gustavo Vasquez <vasquez.mgustavo@gmail.com>
2019-03-01 15:07:54 -0300

    Inhabilitado el botón de maximizar ventana

--- RadioStationApp/RadioStationForm.Designer.cs
+++ RadioStationApp/RadioStationForm.Designer.cs
@@ -173,6 +173,7 @@
             this.Controls.Add(this.btnLaRed);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "RadioStation";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Estación de radio";
