b6072431970231a589b3ee944db1310260446faf
Gustavo Vasquez <vasquez.mgustavo@gmail.com>
2019-03-01 12:35:17 -0300

    Add project files.

---
+++ RadioStationApp/RadioStationForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace RadioStationApp
+{
+    partial class RadioStation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RadioStation));
+            this.btnLaRed = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.Label();
+            this.btnStopStream = new System.Windows.Forms.Button();
+            this.btnContinental = new System.Windows.Forms.Button();
+            this.txtCustomRadio = new System.Windows.Forms.TextBox();
+            this.btnCustomRadio = new System.Windows.Forms.Button();
+            this.imgLaRed = new System.Windows.Forms.PictureBox();
+            this.imgContinental = new System.Windows.Forms.PictureBox();
+            this.imgEqualizer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLaRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgContinental)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgEqualizer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLaRed
+            // 
+            this.btnLaRed.Location = new System.Drawing.Point(47, 177);
+            this.btnLaRed.Name = "btnLaRed";
+            this.btnLaRed.Size = new System.Drawing.Size(75, 23);
+            this.btnLaRed.TabIndex = 0;
+            this.btnLaRed.Text = "LaRed";
+            this.btnLaRed.UseVisualStyleBackColor = true;
+            this.btnLaRed.Click += new System.EventHandler(this.btnLaRed_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 9);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(260, 33);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.Text = "-";
+            this.txtMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStopStream
+            // 
+            this.btnStopStream.Enabled = false;
+            this.btnStopStream.Location = new System.Drawing.Point(73, 227);
+            this.btnStopStream.Name = "btnStopStream";
+            this.btnStopStream.Size = new System.Drawing.Size(129, 23);
+            this.btnStopStream.TabIndex = 2;
+            this.btnStopStream.Text = "Detener";
+            this.btnStopStream.UseVisualStyleBackColor = true;
+            this.btnStopStream.Click += new System.EventHandler(this.btnStopStream_Click);
+            // 
+            // btnContinental
+            // 
+            this.btnContinental.Location = new System.Drawing.Point(155, 177);
+            this.btnContinental.Name = "btnContinental";
+            this.btnContinental.Size = new System.Drawing.Size(75, 23);
+            this.btnContinental.TabIndex = 3;
+            this.btnContinental.Text = "Continental";
+            this.btnContinental.UseVisualStyleBackColor = true;
+            this.btnContinental.Click += new System.EventHandler(this.btnContinental_Click);
+            // 
+            // txtCustomRadio
+            // 
+            this.txtCustomRadio.Location = new System.Drawing.Point(12, 49);
+            this.txtCustomRadio.Name = "txtCustomRadio";
+            this.txtCustomRadio.Size = new System.Drawing.Size(179, 20);
+            this.txtCustomRadio.TabIndex = 4;
+            // 
+            // btnCustomRadio
+            // 
+            this.btnCustomRadio.Location = new System.Drawing.Point(197, 47);
+            this.btnCustomRadio.Name = "btnCustomRadio";
+            this.btnCustomRadio.Size = new System.Drawing.Size(75, 23);
+            this.btnCustomRadio.TabIndex = 5;
+            this.btnCustomRadio.Text = "Escuchar";
+            this.btnCustomRadio.UseVisualStyleBackColor = true;
+            this.btnCustomRadio.Click += new System.EventHandler(this.btnCustomRadio_Click);
+            // 
+            // imgLaRed
+            // 
+            this.imgLaRed.Image = ((System.Drawing.Image)(resources.GetObject("imgLaRed.Image")));
+            this.imgLaRed.ImageLocation = "";
+            this.imgLaRed.Location = new System.Drawing.Point(47, 87);
+            this.imgLaRed.Name = "imgLaRed";
+            this.imgLaRed.Size = new System.Drawing.Size(75, 79);
+            this.imgLaRed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgLaRed.TabIndex = 6;
+            this.imgLaRed.TabStop = false;
+            // 
+            // imgContinental
+            // 
+            this.imgContinental.Image = ((System.Drawing.Image)(resources.GetObject("imgContinental.Image")));
+            this.imgContinental.Location = new System.Drawing.Point(155, 87);
+            this.imgContinental.Name = "imgContinental";
+            this.imgContinental.Size = new System.Drawing.Size(75, 79);
+            this.imgContinental.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgContinental.TabIndex = 7;
+            this.imgContinental.TabStop = false;
+            // 
+            // imgEqualizer
+            // 
+            this.imgEqualizer.Image = ((System.Drawing.Image)(resources.GetObject("imgEqualizer.Image")));
+            this.imgEqualizer.Location = new System.Drawing.Point(127, 122);
+            this.imgEqualizer.Name = "imgEqualizer";
+            this.imgEqualizer.Size = new System.Drawing.Size(23, 14);
+            this.imgEqualizer.TabIndex = 8;
+            this.imgEqualizer.TabStop = false;
+            this.imgEqualizer.Visible = false;
+            // 
+            // RadioStation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.imgEqualizer);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.imgContinental);
+            this.Controls.Add(this.imgLaRed);
+            this.Controls.Add(this.btnCustomRadio);
+            this.Controls.Add(this.txtCustomRadio);
+            this.Controls.Add(this.btnContinental);
+            this.Controls.Add(this.btnStopStream);
+            this.Controls.Add(this.btnLaRed);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RadioStation";
+            this.Text = "Estación de radio";
+            this.Load += new System.EventHandler(this.RadioStation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgLaRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgContinental)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgEqualizer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLaRed;
+        private System.Windows.Forms.Label txtMessage;
+        private System.Windows.Forms.Button btnStopStream;
+        private System.Windows.Forms.Button btnContinental;
+        private System.Windows.Forms.TextBox txtCustomRadio;
+        private System.Windows.Forms.Button btnCustomRadio;
+        private System.Windows.Forms.PictureBox imgLaRed;
+        private System.Windows.Forms.PictureBox imgContinental;
+        private System.Windows.Forms.PictureBox imgEqualizer;
+    }
+}
+
---
+++ RadioStationApp/RadioStationForm.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.WindowsAPICodePack.Taskbar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Core.Interops.Signatures;
+
+namespace RadioStationApp
+{
+    public partial class RadioStation : Form
+    {        
+        private VlcMediaPlayer _vlcMediaPlayer;
+
+        public RadioStation()
+        {
+            InitializeComponent();
+
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            if (currentDirectory == null)
+                return;
+
+            var libDirectory = new DirectoryInfo(Path.Combine(currentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+            _vlcMediaPlayer = new VlcMediaPlayer(libDirectory);
+
+            _vlcMediaPlayer.Playing += VlcMediaPlayerOnPlaying;
+            _vlcMediaPlayer.Stopped += VlcMediaPlayerOnStopped;
+            _vlcMediaPlayer.EncounteredError += VlcMediaPlayerOnEncounteredError;
+        }
+
+        private void VlcMediaPlayerOnEncounteredError(object sender, VlcMediaPlayerEncounteredErrorEventArgs e)
+        {            
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                this.txtMessage.Text = "La url expiró o es incorrecta.";
+            }));
+        }
+
+        private void VlcMediaPlayerOnPlaying(object sender, VlcMediaPlayerPlayingEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate () {
+                this.imgEqualizer.Visible = true;
+                this.btnStopStream.Enabled = true;
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Indeterminate, Handle);
+            }));
+        }
+
+        private void VlcMediaPlayerOnStopped(object sender, VlcMediaPlayerStoppedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate ()
+            {                
+                this.ResetButtonsOfRadioStreams();
+                this.imgEqualizer.Visible = false;
+                this.btnStopStream.Enabled = false;
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Error, Handle);
+                TaskbarManager.Instance.SetProgressValue(100, 100, Handle);
+            }));
+        }
+
+        private void RadioStation_Load(object sender, EventArgs e)
+        {
+            //this.btnLaRed.PerformClick();
+        }
+
+        private void btnLaRed_Click(object sender, EventArgs e)
+        {
+            this.SetRadio("https://la_red.secure2.footprint.net/egress/bhandler/streamroot_lsd2latam/la_red/chunklist_b32768.m3u8", "LaRed AM 910");            
+            btnLaRed.Enabled = false;
+
+            if (!btnContinental.Enabled)
+                btnContinental.Enabled = true;
+        }        
+
+        private void btnContinental_Click(object sender, EventArgs e)
+        {
+            this.SetRadio("https://20823.live.streamtheworld.com/CONTINENTAL_SC", "Continental AM 590");            
+            btnContinental.Enabled = false;
+
+            if (!btnLaRed.Enabled)
+                btnLaRed.Enabled = true;
+        }
+
+        private void btnCustomRadio_Click(object sender, EventArgs e)
+        {
+            this.SetRadio(txtCustomRadio.Text, "personalizada");            
+            this.ResetButtonsOfRadioStreams();
+        }
+
+        private void btnStopStream_Click(object sender, EventArgs e)
+        {            
+            _vlcMediaPlayer.Stop();
+            this.txtMessage.Text = "-";
+        }
+
+        private void SetRadio(string streamUrl, string message)
+        {
+            _vlcMediaPlayer.SetMedia(streamUrl, null);
+            _vlcMediaPlayer.Play();
+            txtMessage.Text = "Estás escuchando radio " + message;            
+        }
+
+        private void ResetButtonsOfRadioStreams()
+        {
+            if (!btnLaRed.Enabled)
+                btnLaRed.Enabled = true;
+            else if (!btnContinental.Enabled)
+                btnContinental.Enabled = true;
+        }        
+    }
+}
